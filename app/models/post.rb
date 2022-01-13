3a6f6fa18c77683696cf83467a986bd90337ff22
Noa Udler <udlernoa4@gmail.com>
2022-01-13 10:40:29 -0800

    commit tree 82c29581f865adfb92fc46385851aa5e636c19bd

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :title_clickbait
+   
+
+   def title_clickbait
+    if title == "True Facts"
+        errors.add(:title, "Can't be this title!")
+    end
+
+
+   end
 end
